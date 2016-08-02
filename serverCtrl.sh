0b2cbbd58d7b1144e24c0eafb3b0e319c22f1556
tecle@126.com <tecle@126.com>
2016-08-02 10:05:37 +0800

    Add stop start function for server control script.

--- serverCtrl.sh
+++ serverCtrl.sh
@@ -1,1 +1,50 @@
-nohup ./build/bin/server > server.logs 2>&1 &
+BASE_PATH=$(cd `dirname $0`; pwd)
+PORT=9876
+SERVER_PID="`netstat -apn | grep ${PORT} | grep LISTEN | awk '{print $7}'|sed 's/\/server//g'`"
+LOG_DIR="/root/logs"
+TAG="`date +%Y-%m-%d`"
+EXT_LIB_PATH="${BASE_PATH}/depend/libs"
+SERVER_BIN="${BASE_PATH}/build/bin/server"
+echo $SERVER_PID
+echo $TAG
+
+exe_cmd(){
+$cmd
+if [ $? -eq 0 ];then
+    echo "Execute cmd [$cmd] done."
+else
+    echo "Execute cmd [$cmd] failed."
+    exit 0
+fi
+}
+
+if [ $1 = "help" ];
+then
+    echo "help: get help information."
+    echo "stop: stop current server."
+    echo "start: start server."
+elif [ $1 = "stop" ];
+then
+    echo "Stop server."
+    if [ -z ${SERVER_PID} ];
+    then
+	echo "Server not running."
+    else
+	cmd="kill -s 10 ${SERVER_PID}"
+	exe_cmd
+    fi
+elif [ $1 = "compile" ];
+then
+    echo "Recompile code."
+    cmd="make clean && make"
+    exe_cmd
+elif [ $1 = "start" ];
+then
+    echo "Start server."
+    if [ -z ${SERVER_PID} ];
+    then
+	cd ${BASE_PATH} && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${EXT_LIB_PATH};nohup ${SERVER_BIN} > server.out 2>&1 &
+    else
+	echo "Server is running."
+    fi
+fi
