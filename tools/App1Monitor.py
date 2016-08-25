b0e67645a635d95feb97c92e7cf51b26134af628
imstudio <tecle@126.com>
2016-08-25 14:14:51 +0800

    add close_wait monitor, try to solve close_wait in server by set socket so_linger opt

--- tools/App1Monitor.py
+++ tools/App1Monitor.py
@@ -15,14 +15,24 @@ class Monitor:
         self.prevDay = 0
         self.detail = []
         self.yesterday = ""
-        self.max_close_wait_num = 0
+        self.close_wait_limit = 30
+        self.last_mail_time = 0
+        self.mail_interval = 3600
 
-    def getCloseWaitCount(self):
-        cmd = "netstat -apn | grep 9876 | grep CLOSE_WAIT | wc -l"
+    def getCloseWaitCount(self, port = 9876):
+        cmd = "netstat -apn | grep %d | grep CLOSE_WAIT | wc -l" % port
         s, o = commands.getstatusoutput(cmd)
         if (s != 0):
             return -1
-        return o
+        return int(o)
+
+    def monitorCloseWait(self):
+        val = self.getCloseWaitCount()
+        if val >= self.close_wait_limit:
+            now = time.time()
+            if now - self.last_mail_time > self.mail_interval:
+                self.sendMail("Too much CLOSE_WAIT!", "Current count: %d\n" % val)
+                self.last_mail_time = now
 
     def getCount(self):
         cmd = "grep %s %s | wc -l" % (GKW, LOG)
@@ -42,28 +52,30 @@ class Monitor:
 
     def summary(self, total):
         incOneDay = total - self.prevDay
-        self.sendMail(incOneDay)
+        #self.sendMail(incOneDay)
         self.prevDay = total
         # dump detail info
         self.detail = []
 
-    def sendMail(self, oneday):
+    def sendMail(self, title,  content):
+        message = MIMEText(content, 'plain', 'utf-8')
+        message['From'] = Header("Monitor", 'utf-8')
+        message['To'] =  Header("Monitor", 'utf-8')
+        message['Subject'] = Header(title, 'utf-8')
+        smtp = smtplib.SMTP()
+        smtp.connect('smtp.exmail.qq.com')
+        smtp.login("licheng@touchtogame.com", "Lc90426")
+        smtp.sendmail("licheng@touchtogame.com", "licheng@touchtogame.com", message.as_string())
+        smtp.quit()
+
+    def sendDailyMonitorMail(self, oneday):
         content = "Hi:\nHere is monitor data:\n" +\
                   "*****************\n\n"+\
                   "total:%d\n\ndetail:%s\n\nhistory:%d\n"\
                   "max CLOSE_WAIT:%d\n" %\
                   (oneday, self.detail, self.prevRound, self.max_close_wait_num);
-        message = MIMEText(content, 'plain', 'utf-8')
-        message['From'] = Header("App1 Monitor", 'utf-8')
-        message['To'] =  Header("Monitor", 'utf-8')
         subject = 'Day monitor data %s.' % self.yesterday
-        message['Subject'] = Header(subject, 'utf-8')
-        smtp = smtplib.SMTP()
-        smtp.connect('smtp.exmail.qq.com')
-        smtp.login("licheng@touchtogame.com", "Lc90426")
-        smtp.sendmail("licheng@touchtogame.com", "593869744@qq.com", message.as_string())
-        smtp.quit()
-
+        self.sendMail(subject, content)
 
     def debug(self):
         self.doItr()
@@ -76,10 +88,11 @@ class Monitor:
 ## 每隔一定时间拉一次数据，记录上次的数据以及本次的数据差值，保存起来
 if __name__ == "__main__":
     m = Monitor()
+    count = 0
     while(True):
         try:
-            m.doItr()
+            m.monitorCloseWait()
         except Exception, e:
             print e
-        time.sleep(1200)
+        time.sleep(30)
 
